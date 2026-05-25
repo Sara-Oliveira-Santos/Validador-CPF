ee213633db8d2761b8bdd22e96db6046ceacc7d2
Sara Oliveira <ctrlsarinha@gmail.com>
2026-05-25 10:37:38 -0400

    segundo commit

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const cpfInput = document.getElementById('cpf');
 
 function validador() {
+    let cpf = obterCpfValido();
+    let cpfArray = cpf.split('').map(Number);
+
+    let pDigito = calcularPrimeiroDigito(cpfArray);
+    let sDigito = calcularSegundoDigito(cpfArray);
+
+    exibirResultado(cpfArray, pDigito, sDigito);
+}
+
+function obterCpfValido() {
     let cpf = cpfInput.value.trim();
 
     while (cpf.length !== 11 || isNaN(cpf)) {
         alert("O CPF precisa ter apenas números e exatamente 11 dígitos.");
-        cpf = prompt("CPF (Apenas números):").trim();
+//        cpf = prompt("CPF (Apenas números):").trim();
     }
+    
+    return cpf;
+}
 
-    let cpfArray = cpf.split('').map(Number);
-
+function calcularPrimeiroDigito(cpfArray) {
     let cont = 10;
     let soma = 0;
     for (let i = 0; i < 9; i++) {
@@ -21,9 +33,12 @@ function validador() {
     if (pDigito > 9) {
         pDigito = 0;
     }
+    return pDigito;
+}
 
-    cont = 11;
-    soma = 0;
+function calcularSegundoDigito(cpfArray) {
+    let cont = 11;
+    let soma = 0;
     for (let i = 0; i < 10; i++) {
         soma += cpfArray[i] * cont;
         cont--;
@@ -33,7 +48,10 @@ function validador() {
     if (sDigito > 9) {
         sDigito = 0;
     }
+    return sDigito;
+}
 
+function exibirResultado(cpfArray, pDigito, sDigito) {
     if (cpfArray[9] === pDigito && cpfArray[10] === sDigito) {
         alert("CPF VÁLIDO");
     } else {
